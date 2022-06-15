d665636d0d0a177d568d5daf9812888ba1db052c
chandana gorupally <107546354+chandanag9009@users.noreply.github.com>
2022-06-15 22:13:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit
 streamlit.title('Hello World')
 
 
-# streamlit.header('Breakfast Menu')
-# streamlit.text('Omega 3 & Blueberry Oatmeal')
-# streamlit.text('Kale, Spinach & Rocket Smoothie')
-# streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 
 
