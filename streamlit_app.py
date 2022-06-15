57092c333019dea38c4f40967daf262f34e36540
chandana gorupally <107546354+chandanag9009@users.noreply.github.com>
2022-06-15 22:17:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,11 +17,9 @@ my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-
-# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-
-
-                      
 streamlit.dataframe(my_fruit_list)
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+
 
 
