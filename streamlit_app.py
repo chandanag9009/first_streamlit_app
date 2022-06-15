b1c07b7e7eaff4d9a37524d280ea600ecdb97928
chandana gorupally <107546354+chandanag9009@users.noreply.github.com>
2022-06-15 22:13:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,23 +2,23 @@ import streamlit
 streamlit.title('Hello World')
 
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+# streamlit.header('Breakfast Menu')
+# streamlit.text('Omega 3 & Blueberry Oatmeal')
+# streamlit.text('Kale, Spinach & Rocket Smoothie')
+# streamlit.text('Hard-Boiled Free-Range Egg')
 
 
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+# streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+# import pandas
 
-my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+# streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 
                       
-streamlit.dataframe(my_fruit_list)
+# streamlit.dataframe(my_fruit_list)
 
 
