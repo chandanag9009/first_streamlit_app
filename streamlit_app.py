e94be3f5499aa81d4cf4315ddddae092ac8a5734
chandana gorupally <107546354+chandanag9009@users.noreply.github.com>
2022-06-16 17:27:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title('Hello World')
 
 
@@ -11,7 +16,7 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+# import pandas
 
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
@@ -31,17 +36,20 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice')
-fruit_choice=streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered', fruit_choice)
+try:
+    fruit_choice=streamlit.text_input('What fruit would you like information about?','Kiwi')
+    if not fruit_choice:
+        streamlit.error("Pkease select a fruit to get information")
+    else:
+         # streamlit.write('The user entered', fruit_choice)
+        # import requests
+        fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+        fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+   except URLError as e:
+    streamlit.error()
 
-import requests
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-
-fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-
-streamlit.dataframe(fruityvice_normalized)
-
-import snowflake.connector
+# import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
